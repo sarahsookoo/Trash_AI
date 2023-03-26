14101bc43739e2527b7ad677cf14a6448a1fca28
abarikdar <73364852+abarikdar@users.noreply.github.com>
2023-03-26 19:37:34 -0400

    Add files via upload

---
+++ website/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
