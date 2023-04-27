5b19747d50dd3958ecad3487c2c58b5308cc1cb6
sarahsookoo <sarah_sookoo@outlook.com>
2023-04-26 20:10:01 -0400

    added dummy data to Trash_AI table

--- connections/connection.py
+++ connections/connection.py
@@ -5,12 +5,27 @@ load_dotenv(dotenv_path = '../.env')
 #import requests
 from dotenv import load_dotenv
 load_dotenv(dotenv_path = './.env')
+from datetime import date
+from boto3.dynamodb.conditions import Attr
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
 
 dynamo_client  =  boto3.resource(service_name = 'dynamodb',region_name = 'us-east-2',
               aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID'),
               aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY'))
 
-product_table = dynamo_client.Table('test')
+product_table = dynamo_client.Table('Trash_AI')
 print(product_table.table_status) #should print ACTIVE if successfully connected
 
-product_table.put_item(Item = {'userID':1,"email":"test@test.com","Password":"testpassword"})
+today = date.today()
+today_string = today.strftime('%m/%d/%Y')
+
+# product_table.put_item(Item = {'Type_of_Trash':'Paper',"Weight": 20,"Date": today_string})
+# product_table.put_item(Item = {'Type_of_Trash':'Paper',"Weight": 30,"Date": today_string})
+# product_table.put_item(Item = {'Type_of_Trash':'Plastic',"Weight": 20,"Date": today_string})
+# product_table.put_item(Item = {'Type_of_Trash':'Plastic',"Weight": 30,"Date": today_string})
+# product_table.put_item(Item = {'Type_of_Trash':'Regular Trash',"Weight": 20,"Date": today_string})
+# product_table.put_item(Item = {'Type_of_Trash':'Regular Trash',"Weight": 30,"Date": today_string})
