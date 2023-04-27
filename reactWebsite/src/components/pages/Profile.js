6ef7c136ddfeddd0f140c211742f6903e258d244
sarahsookoo <sarah_sookoo@outlook.com>
2023-04-26 20:14:36 -0400

    working on showing data visualization

--- reactWebsite/src/components/pages/Profile.js
+++ reactWebsite/src/components/pages/Profile.js
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../Button';
 import './Profile.css';
 
 function Profile() {
 
-    // const [statistics, setStatistics] = useState(null);
+    const [plotPath, setPlotPath] = useState(null);
 
     const handleStatisticsClick = async () => {
-        const response = await fetch('http://127.0.0.1:5000/statistics')
+        const response = await fetch('http://127.0.0.1:5000/statistics');
         const data = await response.json();
-        console.log(data);
+        setPlotPath(data.plot_path);
     }
 
     return (
         <div className='profile'>
-        {/* <h1 className='services'>SERVICES</h1>; */}
-        <video src='/vid5.mp4' autoPlay loop muted />
-        <div className='profile-title'> 
-            <h1>Profile</h1>
-        </div>
-        <div className='account'> 
-            My Account
-        </div>
-        <div className='name'> 
-            Name:
-        </div>
-        <div className='email'> 
-            Email:
-        </div>
+            <video src='/vid5.mp4' autoPlay loop muted />
+            <div className='profile-title'> 
+                <h1>Profile</h1>
+            </div>
+            <div className='account'> 
+                My Account
+            </div>
+            <div className='name'> 
+                Name:
+            </div>
+            <div className='email'> 
+                Email:
+            </div>
             <div className='stats-btn'>
-                    <Button 
+                <Button 
                     className='btns' 
                     button type="submit"
                     buttonStyle='btn--outline'
                     buttonSize='btn--large'
                     onClick={handleStatisticsClick}>
                         My Trash AI Statistics
-                    </Button>
+                </Button>
             </div>
+            {plotPath && 
+                <div className='stats-plot'>
+                    <img src={plotPath} alt='Trash AI Statistics' />
+                </div>
+            }
         </div>
     );
 }
-export default Profile;
 
+export default Profile;
